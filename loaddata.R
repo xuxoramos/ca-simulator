d16b673f450fecc869b9c06e8e99a029126443e1
Jesus Salvador Ramos Cardona <jesus@datank.ai>
2018-05-16 01:48:12 -0500

    Removed incomplete cases. Worked towards viz of clusters.

--- loaddata.R
+++ loaddata.R
@@ -1,24 +1,30 @@
 # Libraries
 library(cluster)
 library(readr)
-library(HSAUR)
 library(tidyverse)  # data manipulation
-library(kamila)
 library(factoextra) # clustering algorithms & visualization
 
 # Load data
 rwasplus <- read_csv('./data/RWAS/modified_data_copy.csv')
 names(rwasplus)[21] <- 'Homosexuals_feminists_praised_for_defy_traditional_values' # clean this particular column
+rwasplus <- rwasplus %>% na.omit
 rwasplus_numeric <- rwasplus %>% select(c(1:32,38,45))
+rwasplus_numeric <- rwasplus_numeric %>% mutate(id = seq(1:nrow(rwasplus_numeric)))
 rwasplus_categorical <- rwasplus %>% select(c(33:37,39:44))
 rwasplus_categorical[names(rwasplus_categorical)] <- lapply(rwasplus_categorical[names(rwasplus_categorical)], factor)
+rwasplus_categorical <- rwasplus_categorical %>% mutate(id = seq(1:nrow(rwasplus_categorical)))
+rwasplus_final <- rwasplus_numeric %>% inner_join(x = rwasplus_numeric, y = rwasplus_categorical, by = 'id')
 
 # k-means with mixed categorical and numeric data
-clusters <- kamila(conVar = rwasplus_numeric, catFactor = rwasplus_categorical, numClust = 3, numInit = 100)
-#k_clusters <- kmeans(rwasplus, centers = 1, iter.max = 100, nstart = 4)
+daisy_dissim_matrix <- daisy(select(rwasplus_final, -46), metric = "gower")
+saveRDS(daisy_dissim_matrix, file = "./data/daisy_dissim_matrix.rdata")
 
-# plot
-fviz_cluster(k_clusters, data = rwasplus)
+# Load dissimilitude matrix
+gower_dissim_matrix <- readRDS(file = "./data/daisy_dissim_matrix.rdata")
+pam_medoids <- pam(x = gower_dissim_matrix, k = 4, diss = T)
+
+# plot IMPORTANT
+#fviz_dist(
 
 # silhouette plot
 dissE <- daisy(rwas) 
